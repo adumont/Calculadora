d00c28be4183d5768cbf22ce306e891b0172fe22
Alexandre Dumont <adumont@gmail.com>
2021-03-30 18:07:59 +0000

    formato pila hp48, con clear

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 import math
 
+import os
+
+# The screen clear function
+def screen_clear():
+   # for mac and linux(here, os.name is 'posix')
+   if os.name == 'posix':
+      _ = os.system('clear')
+   else:
+      # for windows platfrom
+      _ = os.system('cls')
+   # print out some text
+
 pila = []
 
 def showPila():
-  print(pila)
+  l=len(pila)
+  n=min(l,4)
+
+  print("____________________")
+  for i in range(4-n):
+    print("%d:" % (4-i))
+  for i in range(n):
+    num = "%f" % pila[l-n+i]
+    pad = ' ' * (17 - len(num))
+
+    print("%d: %s%s" % (n-i, pad, num))
+  print("")
+    
 
 while True:
+  screen_clear()
   showPila()
   s = input("")
   s = s.strip()
